163e8c422848faf1d9419b042fec45cbd059e891
SaidSuliat <ummelbanaat@duck.com>
2025-03-11 09:56:38 +0100

    second commit

--- WEEK 1/Index.py
+++ WEEK 1/Index.py
@@ -1,1 +1,23 @@
 print("Hello World")
+
+number = 10
+print(number)
+
+
+
+
+
+age = 27 #integer
+marks = 90.5 #float
+
+name = "Zulaykha Hertz" #string
+
+
+
+
+
+# Lists DataType
+
+languages = ["Python", "HTML", "CSS", 3, 5.7, True]
+# print(languages)
+print(languages[-6])
